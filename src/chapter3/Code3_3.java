782f7bbc64bb02a2277349f4abcfadbd27397df1
nobu <algebra.zetafunc@gmail.com>
2020-10-21 07:10:13 +0900

    [add]code3.3

---
+++ src/chapter3/Code3_3.java
@@ -0,0 +1,37 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/**
+ * 最小値を求める
+ * @author nobu
+ *
+ */
+public class Code3_3 {
+
+	private static int INF = 20000000; // 十分大きな値に
+
+	public static void main(String[] args) {
+		// 入力を受け取る
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; ++i) {
+			a[i] = sc.nextInt();
+		}
+
+		// 線形探索
+		int minValue = INF;
+		for (int i = 0; i < n; ++i) {
+			if (a[i] < minValue) {
+				minValue = a[i];
+			}
+		}
+
+		// 結果出力
+		System.out.println(minValue);
+
+		sc.close();
+	}
+
+}
